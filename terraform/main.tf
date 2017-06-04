67fe67644b20188547337d84bc63598a6670bcb3
Bill Hegazy <bill.hegazy@mbww.com>
2017-06-05 06:26:55 +0800

    using vars on names and more

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,24 +6,36 @@ data "aws_availability_zones" "az" {}
 
 variable "nginx_port" {
   description = "The port the server will use for HTTP requests"
-  default = 8080
+  default = 80
 }
 
 variable "instance_type" {
   default = "t2.micro"
 }
 
-variable "size" {
+variable "num_servers" {
   default = 1
 }
 
+variable "environment" {
+  default = "dev"
+}
+
+variable "app_name" {
+  default = "hello"
+}
+
+variable "ssh_key" {
+  default = "test"
+}
+
 resource "aws_key_pair" "bhegazy" {
-  key_name   = "bhegazy-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDXFxpJFAXb8e3U7QnfPc7GdbXX9LzWDhrTo4Vh9O4z2yJyDKgmS3Ffj1yyHZ8Fw8frI5D9emdN7ZEgANUNx6aDJCTFNZb0AqF/3QwkezApzLixYfwh4wv7oDqcmSXho9JFB7GhYH3B8CjEEf+GW1qoXoStfSYDyozlhcJy1jekWmQF3tY4VSiMNV+yiSIu6iBHGYjVgLidQpDxmZOnkikc85lkQ1RcZ9NLoCdvSUSAL4dLsgvDlxwStPARWP/QW1W5+i7h9Y+saN5L35vbkAadwrENx+KTp2pfgnoAZMO/I8iESu7a/4zL/EpATvFDBcH+CZLEPHvyzQizQwfrT74vaYXGNq+r5eoFYQdpHaHtKbb5stFWAjVwfcTndNnCLlGdIefbrTcusrcSAD6t3e+iO9G4pebAQtkSNesKhs8cZaOGaXb2UxkAUV/YwOG9a8RHTs2Nz4oaPRFxQAinip5/N/FH+PVwoglZmPaGVhGgI7t0Mumvr7JJ2bHzGCD8stiMvpbIfMnVomoYeTEWnyDQNNu7r9mwy6Sy7R/j02w18QOZ71r/rR9cDxwrOFoHjmZJ7gvd06hBRhH/JarbCQpPanGcjP4NeI46PIA0ujvv4yCriL/ffYL/AZzLs8vqzoQdlwk7I9T9NrwL2M5LQiBnUBFOvEYzvAjcqiLqEdOqTw=="
+  key_name   = "${var.app_name}-${var.environment}"
+  public_key = "${var.ssh_key}"
 }
 
 resource "aws_security_group" "main-sg" {
-  name = "bhegazy-instance-sg"
+  name = "${var.app_name}-${var.environment}-instance-sg"
 
   ingress {
     from_port = "${var.nginx_port}"
@@ -51,7 +63,7 @@ resource "aws_security_group" "main-sg" {
 }
 
 resource "aws_security_group" "elb-sg" {
-  name = "bhegazy-elb-sg"
+  name = "${var.app_name}-${var.environment}-elb-sg"
   
   egress {
     from_port = 0
@@ -69,13 +81,14 @@ resource "aws_security_group" "elb-sg" {
 }
 
 resource "aws_instance" "bhegazy" {
-  count = "${var.size}"
+  count = "${var.num_servers}"
   ami = "ami-772aa961"
   instance_type = "${var.instance_type}"
   vpc_security_group_ids = ["${aws_security_group.main-sg.id}"]
   key_name = "${aws_key_pair.bhegazy.key_name}"
   tags {
-    Name = "bhegazy"
+    Name = "${var.app_name}-${var.environment}"
+    Environment = "${var.environment}"
   }
   lifecycle {
     create_before_destroy = true
@@ -83,12 +96,12 @@ resource "aws_instance" "bhegazy" {
 }
 
 resource "aws_elb" "bhegazy" {
-  name = "bhegazy"
+  name = "${var.app_name}-${var.environment}"
   availability_zones = ["${data.aws_availability_zones.az.names}"]
   security_groups = ["${aws_security_group.elb-sg.id}"]
-
+  instances = ["${aws_instance.bhegazy.*.id}"]
   health_check {
-    target = "HTTP:${var.nginx_port}/"
+    target = "TCP:${var.nginx_port}"
     healthy_threshold = 5
     unhealthy_threshold = 5
     timeout = 6
@@ -107,4 +120,7 @@ output "public_ip" {
   value = "${aws_instance.bhegazy.*.public_ip}"
 }
 
+output "elb_dns_name" {
+  value = "${aws_elb.bhegazy.dns_name}"
+}
 
