7867e555cd65e09868f7b6d25d10382754df634a
Bill Hegazy <bill.hegazy@mbww.com>
2017-06-05 02:26:33 +0800

    use ec2 instead of ASG

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,8 +13,13 @@ variable "instance_type" {
   default = "t2.micro"
 }
 
-variable "min_size" {
+variable "size" {
   default = 1
+}
+
+resource "aws_key_pair" "bhegazy" {
+  key_name   = "bhegazy-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDXFxpJFAXb8e3U7QnfPc7GdbXX9LzWDhrTo4Vh9O4z2yJyDKgmS3Ffj1yyHZ8Fw8frI5D9emdN7ZEgANUNx6aDJCTFNZb0AqF/3QwkezApzLixYfwh4wv7oDqcmSXho9JFB7GhYH3B8CjEEf+GW1qoXoStfSYDyozlhcJy1jekWmQF3tY4VSiMNV+yiSIu6iBHGYjVgLidQpDxmZOnkikc85lkQ1RcZ9NLoCdvSUSAL4dLsgvDlxwStPARWP/QW1W5+i7h9Y+saN5L35vbkAadwrENx+KTp2pfgnoAZMO/I8iESu7a/4zL/EpATvFDBcH+CZLEPHvyzQizQwfrT74vaYXGNq+r5eoFYQdpHaHtKbb5stFWAjVwfcTndNnCLlGdIefbrTcusrcSAD6t3e+iO9G4pebAQtkSNesKhs8cZaOGaXb2UxkAUV/YwOG9a8RHTs2Nz4oaPRFxQAinip5/N/FH+PVwoglZmPaGVhGgI7t0Mumvr7JJ2bHzGCD8stiMvpbIfMnVomoYeTEWnyDQNNu7r9mwy6Sy7R/j02w18QOZ71r/rR9cDxwrOFoHjmZJ7gvd06hBRhH/JarbCQpPanGcjP4NeI46PIA0ujvv4yCriL/ffYL/AZzLs8vqzoQdlwk7I9T9NrwL2M5LQiBnUBFOvEYzvAjcqiLqEdOqTw=="
 }
 
 resource "aws_security_group" "main-sg" {
@@ -24,6 +29,20 @@ resource "aws_security_group" "main-sg" {
     from_port = "${var.nginx_port}"
     to_port = "${var.nginx_port}"
     protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   lifecycle {
@@ -49,29 +68,17 @@ resource "aws_security_group" "elb-sg" {
   }
 }
 
-resource "aws_launch_configuration" "bhegazy" {
-  image_id = "ami-772aa961"
+resource "aws_instance" "bhegazy" {
+  count = "${var.size}"
+  ami = "ami-772aa961"
   instance_type = "${var.instance_type}"
-  security_groups = ["${aws_security_group.main-sg.id}"]
+  vpc_security_group_ids = ["${aws_security_group.main-sg.id}"]
+  key_name = "${aws_key_pair.bhegazy.key_name}"
+  tags {
+    Name = "bhegazy"
+  }
   lifecycle {
     create_before_destroy = true
-  }
-}
-
-resource "aws_autoscaling_group" "bhegazy" {
-  launch_configuration = "${aws_launch_configuration.bhegazy.id}"
-  availability_zones = ["${data.aws_availability_zones.az.names}"]
-
-  load_balancers = ["${aws_elb.bhegazy.name}"]
-
-  min_size = "${var.min_size}"
-  max_size = 10
-  health_check_type = "ELB"
-  
-  tag {
-    key = "Name"
-    value = "bhegazy-asg"
-    propagate_at_launch = true
   }
 }
 
@@ -95,4 +102,9 @@ resource "aws_elb" "bhegazy" {
     instance_protocol = "http"
   }
 }
+
+output "public_ip" {
+  value = "${aws_instance.bhegazy.*.public_ip}"
+}
+
 
